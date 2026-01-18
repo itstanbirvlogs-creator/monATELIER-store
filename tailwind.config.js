7a84d714c26bacac3fe51da1690c2f9d4860762f
itstanbirvlogs-creator <itstanbirvlogs@gmail.com>
2026-01-18 23:38:38 +0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'intl-blue': '#002366',
+        'intl-slate': '#1e293b'
+      }
+    },
+  },
+  plugins: [],
+}
